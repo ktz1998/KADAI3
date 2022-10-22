a365c815067f81f28fc21911e93e2d357b7dbf60
ktz1998 <doraemonfan.kosuke@gmail.com>
2022-10-22 09:30:04 +0000

    End アプリケーションに必要なファイルの準備

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,4 +75,6 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   config.hosts << "4d1408d649fc4736a7d6977ef8a3f057.vfs.cloud9.ap-northeast-1.amazonaws.com"
+  
+  config.active_job.queue_adapter = :inline
 end
